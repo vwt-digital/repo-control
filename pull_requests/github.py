93f3d2b2282a451fe8c4dcd10ab02a70d1ed38b6
j.vanenckevort <j.vanenckevort@vwt.digital>
2020-08-11 14:45:51 +0200

    DAT-3946. Add script to create pull requests per organization

---
+++ pull_requests/github.py
@@ -0,0 +1,67 @@
+import logging
+import argparse
+
+from github import Github
+from github import GithubException
+from collections import Counter
+
+logging.basicConfig(level=logging.INFO, format='%(levelname)7s: %(message)s')
+
+
+def create_pull_request(repo):
+    """Creates a pull request from develop to master"""
+
+    try:
+        return repo.create_pull(title="Development to production", body="", head="develop", base="master")
+    except GithubException:
+        print("A pull request already exist for this repo {}".format(repo.name))
+
+
+def get_most_common(list):
+    """Returns the most common element from a list"""
+
+    counter = Counter(list)
+    common = counter.most_common(1)
+
+    return common[0][0]
+
+
+def parse_args():
+    """A simple function to parse command line arguments"""
+
+    parser = argparse.ArgumentParser(description='Create pull request for a Github organization')
+    parser.add_argument('-t', '--token',
+                        required=True,
+                        help='github access token')
+    parser.add_argument('-o', '--organization',
+                        required=True,
+                        help='github organization name')
+    return parser.parse_args()
+
+
+def main(args):
+
+    git = Github(args.token)
+    org = git.get_organization(args.organization)
+
+    for repo in org.get_repos():
+
+        logging.info("Checking repo {}".format(repo.name))
+
+        pull_request = create_pull_request(repo)
+
+        if pull_request:
+
+            logging.info(" + Opened pull request")
+
+            commits = pull_request.get_commits()
+            authors = [commit.author.login for commit in commits]
+            assignee = get_most_common(authors)
+
+            pull_request.create_review_request(reviewers=[assignee])
+
+            logging.info(" + Assigned {} to pull request".format(assignee))
+
+
+if __name__ == '__main__':
+    main(parse_args())
